1ad12580661d4f702192175c9398a39589b9653d
BuildTools <unconfigured@null.spigotmc.org>
2016-09-22 21:30:22 -0500

    Character Creation

--- src/interpreter.c
+++ src/interpreter.c
@@ -1757,7 +1757,10 @@ void nanny(struct descriptor_data *d, char *arg)
     echo_on(d);
 
     if (STATE(d) == CON_CNFPASSWD) {
+        write_to_output(d, "\r\n");
+        write_to_output(d, "\r\n");
       write_to_output(d, "\r\nPress Enter. ");
+      write_to_output(d, "\r\n");
 //     STATE(d) = CON_QSEX;
         SET_BIT_AR(PLR_FLAGS(d->character), PLR_NEW);
         STATE(d) = CON_MENU;
